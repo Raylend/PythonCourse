ea6ebce4325e4698dde2ca291b4aba0b8c16a597
Raylend <36769316+Raylend@users.noreply.github.com>
2019-03-30 01:25:08 +0300

    Transcribing DNA into RNA
    Problem
    An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
    
    Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
    
    Given: A DNA string t having length at most 1000 nt.
    
    Return: The transcribed RNA string of t.

---
+++ hw_B1.py
@@ -0,0 +1,26 @@
+"""
+Transcribing DNA into RNA
+
+Problem
+An RNA string is a string formed from the alphabet containing 'A', 'C',
+'G', and 'U'.
+
+Given a DNA string t corresponding to a coding strand, its transcribed
+RNA string u is formed by replacing all occurrences of 'T' in t with 'U'
+in u.
+
+Given: A DNA string t having length at most 1000 nt.
+
+Return: The transcribed RNA string of t.
+"""
+s = input("Enter your DNA string and I will transform it into the RNA string\n")
+t = ""
+for element in s:
+    if element=='T':
+        t = t + 'U'
+    else:
+        t = t + element
+
+print("RNA string")
+answer = t
+print(t)
